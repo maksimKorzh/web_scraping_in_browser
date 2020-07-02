dfe230989448c782cbe197b21ec4a6a53df9b4e0
Maksim Korzh <freesoft.for.people@gmail.com>
2020-07-02 19:38:28 +0300

    added pagination crawler tutorial

---
+++ src/pagination_crawling/pagination.js
@@ -0,0 +1,45 @@
+/***********************************************\
+            Script to crawl through
+                 multiple pages
+               
+                       by
+                       
+                Code Monkey King
+              
+\***********************************************/
+
+// import JQuery
+var jq = document.createElement('script');
+jq.src = 'https://code.jquery.com/jquery-3.5.1.min.js';
+document.getElementsByTagName('head')[0].append(jq)
+
+// pagination crawler method
+function crawlNextPage() {
+  // open URL in a new window
+  var response = window.open('http://quotes.toscrape.com', 'new', true);
+
+  // init window onload function
+  response.onload = function crawlNextPage() {
+    setTimeout(function () {
+      if ($(response.document).find('li[class="next"] a')[0]) {  
+        // print debug info
+        console.log('crawling next page...');
+        
+        // data extraction logic
+        var quote = $(response.document).find('span[class="text"]').text();
+        console.log(quote);
+
+        //extract and click next page URL
+        $(response.document).find('li[class="next"] a')[0].click();
+
+        // crawl pagination recursively
+        crawlNextPage();
+      } else {
+        console.log('All done!');
+      }
+    },1000) // crawl delay
+  };
+}
+
+// wait until JQuery is loaded
+setTimeout(crawlNextPage(), 1000)
